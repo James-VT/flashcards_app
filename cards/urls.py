71ee1abebd0c3a123819bffb1fac79a51c855cf9
James-VT <jd_tanner@hotmail.co.uk>
2022-07-28 17:20:14 +0100

    Creates template and backend for card boxes

--- cards/urls.py
+++ cards/urls.py
@@ -19,5 +19,13 @@ urlpatterns = [
         "edit/<int:pk>",
         views.CardUpdateView.as_view(),
         name="card-update"
-    )
+    ),
+    # The below is where the box_num keyword argument comes from.
+    # Presumably it's generated from the URL or some built-in Django thing,
+    # like the primary key above.
+    path(
+        "box/<int:box_num>",
+        views.BoxView.as_view(),
+        name="box"
+    ),
 ]
