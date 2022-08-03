c808104b132701bd2f144954322aa6368ee4622f
James-VT <jd_tanner@hotmail.co.uk>
2022-08-03 18:15:33 +0100

    Reached the end of the tutorial

--- cards/views.py
+++ cards/views.py
@@ -7,7 +7,11 @@ from django.views.generic import (
     UpdateView,
 )
 
+from django.shortcuts import get_object_or_404, redirect
+
 import random
+
+from .forms import CardCheckForm
 
 from .models import Card
 
@@ -25,6 +29,7 @@ class CardUpdateView(CardCreateView, UpdateView):
 
 class BoxView(CardListView):
     template_name = "cards/box.html"
+    form_class = CardCheckForm
 
     def get_queryset(self):
         return Card.objects.filter(box=self.kwargs["box_num"])
@@ -35,3 +40,11 @@ class BoxView(CardListView):
         if self.object_list:
             context["check_card"] = random.choice(self.object_list)
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            card = get_object_or_404(Card, id=form.cleaned_data["card_id"])
+            card.move(form.cleaned_data["solved"])
+
+        return redirect(request.META.get("HTTP_REFERER"))
